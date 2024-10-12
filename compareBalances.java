0e2baa9e3778742aea63c45d96be9629dcb703de
Hogan Messinger <messinger8771@gmail.com>
2024-10-12 16:48:18 -0500

    "Added a rule to compareBalances"

--- compareBalances.java
+++ compareBalances.java
@@ -1,7 +1,7 @@
 
 /**
  * This class compares the user's savings and checkings balances. It then
- * returns a message based on the comparison. This class impelments the rule:
+ * returns a message based on the comparison. This class implements the rule:
  * EXP03-J: Do not use the equality operators when comparing values of boxed
  * primitives.
  * 
@@ -13,14 +13,23 @@ public class compareBalances {
 
     /**
      * This method compares the savings and checkings balances of a user's account.
+     * It uses the .equals method to compare the boxed primitives rather than using
+     * the equality operators. This follows the rule: EXP03-J: Do not use the
+     * equality operators when comparing values of boxed
+     * primitives.
+     * 
+     * Instead of using the deprecated Double constructor class, we used the method
+     * Double.valueOf method to turn the double values into boxed Doubles.
+     * This implements the rule: MET02-J. Do not use deprecated or obsolete classes
+     * or methods.
      * 
      * @param account - The user account.
      */
     public void compareBalances(BankAccount account) {
-        Double savings = new Double(account.getSavingsBalance());
-        Double checkings = new Double(account.getCheckingsBalance());
+        Double savings = Double.valueOf(account.getSavingsBalance());
+        Double checkings = Double.valueOf(account.getCheckingsBalance());
 
-        // This if else-if else statement follows these recommendatins:
+        // This if else-if else statement follows these recommendations:
         // EXP51-J: Do not perform assignments in conditional expressions.
         // EXP52-J: Use braces for the body of an if, for, or while statement.
         if (savings.equals(checkings)) {
