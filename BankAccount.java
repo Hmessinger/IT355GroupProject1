1d6d83ab1d9bdf36c8f8da996cb3d8f6a5e51559
Hogan Messinger <messinger8771@gmail.com>
2024-10-12 16:15:09 -0500

    Made some adjustments to BankAccount

--- BankAccount.java
+++ BankAccount.java
@@ -18,13 +18,14 @@ class BankAccount {
     private final String name;
     private final int customerId;
     private final int accountNumber;
-    private final double savingsBalance;
-    private final double checkingsBalance;
+    private double savingsBalance;
+    private double checkingsBalance;
+    private String password;
 
     // NUM10-J. Do not construct BigDecimal objects from floating-point literals
     private BigDecimal intrestRate = new BigDecimal("0.000000001");
 
-    // private final String password = "hello";
+    // Constructor
     public BankAccount(String name, int customerId, int accountNumber, double savingsBalance, double checkingsBalance) {
         this.checkingsBalance = checkingsBalance;
         this.savingsBalance = savingsBalance;
@@ -33,7 +34,55 @@ class BankAccount {
         this.customerId = customerId;
     }
 
-    // MET09-J: Classes that define an equals() method must also define a hashCode() method
+    // Overloaded Constructor
+    public BankAccount(String name, int customerId, int accountNumber, double savingsBalance, double checkingsBalance,
+            String password) {
+        this.checkingsBalance = checkingsBalance;
+        this.savingsBalance = savingsBalance;
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.customerId = customerId;
+        this.password = password;
+    }
+
+    public void setSavingsBalance(double savingsBalance) {
+        this.savingsBalance = savingsBalance;
+    }
+
+    public void setCheckingsBalance(double checkingsBalance) {
+        this.checkingsBalance = checkingsBalance;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getSavingsBalance() {
+        return savingsBalance;
+    }
+
+    public double getCheckingsBalance() {
+        return checkingsBalance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // MET09-J: Classes that define an equals() method must also define a hashCode()
+    // method
     @Override
     public boolean equals(Object accountObject) {
         if (this == accountObject) {
@@ -46,7 +95,8 @@ class BankAccount {
         return customerId == account.customerId && accountNumber == account.accountNumber;
     }
 
-    // MET09-J: Classes that define an equals() method must also define a hashCode() method
+    // MET09-J: Classes that define an equals() method must also define a hashCode()
+    // method
     @Override
     public int hashCode() {
         return Objects.hash(accountNumber, customerId);
@@ -62,7 +112,8 @@ class BankAccount {
     }
 
     // MSC04-J: Do not leak memory
-    // Keeping a file open for too long can lead to memory leaks because file handles (system resources) are limited,
+    // Keeping a file open for too long can lead to memory leaks because file
+    // handles (system resources) are limited,
     // and each open file consumes memory.
     public void createAccountLogFile() throws IOException {
         BankAccount.clearAccountLogFile();
@@ -77,9 +128,11 @@ class BankAccount {
 
         String fileName = dirPath.resolve(this.accountNumber + "-logfile.txt").toString();
 
-        try (FileOutputStream fos = new FileOutputStream(fileName, true); DataOutputStream dos = new DataOutputStream(fos)) {
+        try (FileOutputStream fos = new FileOutputStream(fileName, true);
+                DataOutputStream dos = new DataOutputStream(fos)) {
 
-            // FIO09-J. Do not rely on the write() method to output integers outside the range 0 to 255
+            // FIO09-J. Do not rely on the write() method to output integers outside the
+            // range 0 to 255
             dos.writeInt(this.accountNumber);
         } catch (IOException e) {
         }
@@ -103,9 +156,13 @@ class MassAccountCreate {
     public void createAccountsUsingFile(String inputFileName) {
 
         // MET12-J: Do not use finalizers
-        // The code avoids using finalizers by leveraging the try-with-resources construct.
-        // Try-with-resources block automatically handles resource managemen  for any object 
-        try (FileInputStream inputFile = new FileInputStream(inputFileName); InputStreamReader inputStreamReader = new InputStreamReader(inputFile); BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+        // The code avoids using finalizers by leveraging the try-with-resources
+        // construct.
+        // Try-with-resources block automatically handles resource management for any
+        // object
+        try (FileInputStream inputFile = new FileInputStream(inputFileName);
+                InputStreamReader inputStreamReader = new InputStreamReader(inputFile);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
 
             String line;
             ArrayList<BankAccount> accounts = new ArrayList<>();
@@ -119,7 +176,8 @@ class MassAccountCreate {
                     double savingsBalance = Double.parseDouble(words[3]);
                     double checkingsBalance = Double.parseDouble(words[4]);
 
-                    accounts.add(new BankAccount(words[0], customerId, accountNumber, savingsBalance, checkingsBalance));
+                    accounts.add(
+                            new BankAccount(words[0], customerId, accountNumber, savingsBalance, checkingsBalance));
 
                 } catch (NumberFormatException e) {
                     System.out.println("Error parsing number: " + e.getMessage());
