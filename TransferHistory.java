a1408816d4faee16e9cb6ed0f930179d00dc2ec1
Hogan Messinger <messinger8771@gmail.com>
2024-10-12 15:53:45 -0500

    Changed the viewHistory method.

--- TransferHistory.java
+++ TransferHistory.java
@@ -1,24 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class TransferHistory {
-    
+
     private final List<String> history = new ArrayList<>();
 
-    //Add transfer record to history
-    public void addRecord(String record){
-        //MET00-J: Validate method arguments
-        if(record == null || record.isEmpty()){
+    // Add transfer record to history
+    public void addRecord(String record) {
+        // MET00-J: Validate method arguments
+        if (record == null || record.isEmpty()) {
             throw new IllegalArgumentException("Record cannot be null or empty.");
         }
         history.add(record);
     }
 
-    public void viewHistory(){
-        if(history.isEmpty()){
-            System.out.println("No transfer found.");
-        }
-        else{
-            history.forEach(System.out::println);
-        }
+    /**
+     * In this method, rather than returning the private mutable history List, we
+     * create a copy of it and return the copied List. This method will return the
+     * list even if it is an empty list.
+     * This method implements the following rules and recommendations:
+     * OBJ05-J: Do not return references to private mutable class members.
+     * MET55-J: Return an empty array or collection instead of a null value for the
+     * methods that return an array or collection.
+     */
+    public List<String> viewHistory() {
+        return new ArrayList<>(history);
+
+        // if (history.isEmpty()) {
+        // System.out.println("No transfer found.");
+        // } else {
+        // history.forEach(System.out::println);
+        // }
     }
 }
